674247a868f7981c5c2bc8308ca122e8357eb8de
Scoowy <gahonajuanjo@gmail.com>
2018-07-01 00:51:56 -0500

    Cambios

--- js/cargaArchivos.js
+++ js/cargaArchivos.js
@@ -1,32 +1,42 @@
+// $.get("lojatienefuturo-users-locates.csv", function(csvString) { 
+//     var data = $.csv.toObjects(csvString);
+//     console.log(data);
+//  });
 
-$.get("lojatienefuturo-users-locates.csv", function(csvString) { 
-    var data = $.csv.toObjects(csvString);
-    console.log(data);
- });
+// fetch('lojatienefuturo-users-locates.csv')
+//     .then(function (response) {
+//         return response.blob();
+//     })
+//     .then(function (myBlob) {
+//         var objectURL = URL.createObjectURL(myBlob);
+//         console.log(objectURL);
+//         var data = $.csv.toObjects(objectURL.blob);
+//         console.log(data);
+//     });
 
 // var data = $.csv.toObjects("lojatienefuturo-users-locates.csv");
 
 // console.log(data);
-// $(document).ready(function () {
-//     $.ajax({
-//         type: "GET",
-//         url: "lojatienefuturo-users-locates.csv",
-//         dataType: "text",
-//         success: function(data) {
-//             csv2list(data);
-//         }
-//     });
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "lojatienefuturo-users-locates.csv",
+        dataType: "text",
+        success: function(data) {
+            csv2list(data);
+        }
+    });
 
-//     function csv2list(data) {
-//         var filas = data.split(/\r\n|\r/);
-//         var latLongList = [];
+    function csv2list(data) {
+        var filas = data.split(/\r\n|\r/);
+        var latLongList = [];
 
-//         for (let fila = 1; fila < filas.length; fila++) {
-//             var columnas = filas[fila].split(",");
-//             for (let columna = 1; columna < columnas.length; columna++) {
-//                 const element = columnas[columna];
-//                 console.log(element);
-//             }
-//         }
-//     };
-// });
+        for (let fila = 1; fila < filas.length; fila++) {
+            var columnas = filas[fila].split(",");
+            for (let columna = 1; columna < columnas.length; columna++) {
+                const element = columnas[columna];
+                console.log(element);
+            }
+        }
+    };
+});
